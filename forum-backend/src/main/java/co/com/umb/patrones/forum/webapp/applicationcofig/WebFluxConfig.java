24f541b06a4ecd4e89222a76e2d2e4e2fd67d1db
daniel-buritica <buritica_1999@hotmail.com>
2022-11-19 17:59:03 -0500

    add cors

---
+++ forum-backend/src/main/java/co/com/umb/patrones/forum/webapp/applicationcofig/WebFluxConfig.java
@@ -0,0 +1,30 @@
+package co.com.umb.patrones.forum.webapp.applicationcofig;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+
+@Configuration
+public class WebFluxConfig {
+
+
+    @Bean
+    public WebFluxConfigurer corsConfigure(){
+        return new WebFluxConfigurerComposite(){
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+}
+
